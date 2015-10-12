03c0a666d97275ade3e069e1df8615540f3ebcbf
Filip Bruun Bech-Larsen <fbruun@skybrud.dk>
2015-10-12 10:43:18 +0200

    Initial commit

---
+++ sky-path.provider.ts
@@ -0,0 +1,36 @@
+(function() {
+	'use strict';
+
+	angular.module('skyPath').provider('skyPath', skyPathProvider);
+
+	function skyPathProvider():any {
+		// Determins if on local domain or not
+		var isLocal = (/(ess-food|testserver)/i).test(window.location.host);
+
+		// The base path
+		var path = isLocal ? '' : 'http://5897es.stage.testserver.nu';
+
+		/*
+		 * Change the local state
+		 */
+		this.setLocal = function(local) {
+			isLocal = local;
+		};
+
+		/*
+		 * Change the path
+		 */
+		this.setPath = function(newPath) {
+			path = newPath;
+		};
+
+		this.$get = function() {
+			return {
+				get: function() {
+					return path;
+				}
+			};
+		};
+	}
+
+})();
